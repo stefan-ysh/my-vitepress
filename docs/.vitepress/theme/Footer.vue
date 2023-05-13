5f77e122dfd06f2bb8005e26ad1d6a48a0ce24bb
yshuai <58702199+stefan-ysh@users.noreply.github.com>
2023-05-13 23:48:44 +0800

    feat:add footer component file

---
+++ docs/.vitepress/theme/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="footer">CC BY-NC-SA 4.0 2021-PRESENT © Anthony Fu</div>
+</template>
+<style lang="less">
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 3rem;
+  text-align: center;
+}
+</style>
